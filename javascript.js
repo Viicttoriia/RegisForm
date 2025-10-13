a52c4d5fd0e0bbb0cc6855711b96ec9713104624
VictoriaWiley <victoriawiley1992@gmail.com>
2025-10-13 11:56:56 -0500

    updating layout

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.getElementById("index.html").onsubmit = function(event) {
+    event.preventDefault();
+    let name = document.getElementById("firstName").value + " " + document.getElementById("lastName").value;
+    document.getElementById("output").innerHTML = `<p>Name: ${name}</p>`;
+}
+
+function(event) {
+    event.preventDefault();
+    let Email = document.getElementById("email").value;
+    document.getElementById("output").innerHTML = `<p>email: ${email}</p>`;
+}
+
