8137f265721ea7fddac9c0982769817a57fea9bb
VictoriaWiley <victoriawiley1992@gmail.com>
2025-10-15 17:26:17 -0500

    start over java

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const outputElement = document.getElementById('outputArea')
-
-document.getElementById("index.html").onsubmit = function(event) {
-    event.preventDefault();
-    let name = document.getElementById("firstName").value + " " + document.getElementById("lastName").value;
-    document.getElementById("output").innerHTML = `<p>Name: ${name}</p>`;
-}
-
-function(event) {
-    event.preventDefault();
-    let email = document.getElementById("email").value;
-    document.getElementById("output").innerHTML = `<p>email: ${email}</p>`;
-}
 
